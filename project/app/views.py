9b06fa1abf3636f5201dbc772139a6e1f7b8f9b1
smritijaiswal2007 <smritijais999@gmail.com>
2025-11-29 23:41:43 +0530

    new

--- project/app/views.py
+++ project/app/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def landing(req):
-    # data = {'name':'rohit','age':'2','city':'bhopal'}
+    context= {'name':'rohit','age':'2','city':'bhopal'}
     l = {'smriti','rohit','priyesh','shubh','mom''dad'}
-    return render(req,'landing.html')
+    return render(req,'landing.html',context)
